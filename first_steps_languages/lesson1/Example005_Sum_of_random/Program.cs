7477b1bd60192bbe77dba5564f4c22b457635c49
ins1der23 <m.zadorin@uopt.ru>
2023-02-10 18:16:23 +0500

    First steps languages created

---
+++ first_steps_languages/lesson1/Example005_Sum_of_random/Program.cs
@@ -0,0 +1,3 @@
+﻿int a = NewRandom.Next(1,20);
+int b = NewRandom.Next(1,20);
+Console.WriteLine(a+b);
---
+++ first_steps_languages/practice1/example003/Program.cs
@@ -0,0 +1,16 @@
+﻿// 3. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+//    a = 25, b = 5 -> да 
+//    a = 2, b = 10 -> нет 
+//    a = 9, b = -3 -> да 
+//                                    a = -3 b = 9 -> нет
+
+double a = double.Parse(Console.ReadLine());
+double b = double.Parse(Console.ReadLine());
+if(a==b*b)
+{
+    Console.WriteLine("Yes");
+}
+else 
+{
+    Console.WriteLine("No");
+}
