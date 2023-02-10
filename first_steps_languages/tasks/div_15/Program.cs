a83beb1c858b110cb260028b083d402245bf0be0
ins1der23 <m.zadorin@uopt.ru>
2023-02-11 00:09:10 +0500

    Div for 7 and 23 added

---
+++ first_steps_languages/tasks/div_15/Program.cs
@@ -0,0 +1,26 @@
+﻿Random rnd = new Random();
+int x = rnd.Next(1,9999);
+Console.WriteLine(x);
+if (x % 7 == 0)
+{
+    x = x / 7;
+    if (x % 23 == 0)
+    {
+        Console.WriteLine("Число кратно 7 и 23");
+    }
+    else
+    {
+        Console.WriteLine("Число кратно 7");
+    }
+}
+else
+{
+    if (x % 23 == 0)
+    {
+        Console.WriteLine("Число кратно 23");
+    }
+    else
+    {
+        Console.WriteLine("Число не кратно 7 и 23");
+    }
+}
