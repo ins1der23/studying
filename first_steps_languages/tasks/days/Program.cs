dfc201e6898ead722b2d256dbbcf64821a17c525
ins1der23 <m.zadorin@uopt.ru>
2023-02-11 14:28:58 +0500

    Added days in the tasks

---
+++ first_steps_languages/tasks/days/Program.cs
@@ -0,0 +1,19 @@
+﻿int n = 8;
+while (n > 7 || n<1)
+{
+Console.WriteLine("Введите номер дня недели и я опрелю выходный ли это");
+n = int.Parse(Console.ReadLine());
+Console.WriteLine("Не является днем недели");
+}
+    if (n > 0 && n < 8)
+    {
+        if (n == 6 || n == 7)
+        {
+            Console.WriteLine("Выходной");
+        }
+        else
+        {
+            Console.WriteLine("Будний день");
+        }
+    }
+    
