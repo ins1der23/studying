4c957e8c733c8c8c88e7ea29b2936125c11f7af5
ins1der23 <m.zadorin@uopt.ru>
2023-02-11 15:28:09 +0500

    Squares added in tasks

---
+++ first_steps_languages/tasks/squares/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите два числа, чтобы проверить является ли одно квалратом другого");
+double x = double.Parse(Console.ReadLine());
+double y = double.Parse(Console.ReadLine());
+if (Math.Sqrt(x)==y || Math.Sqrt(y)==x)
+{
+if (Math.Sqrt(x)==y)
+{
+    Console.WriteLine("{0} является квадратом {1}",x,y);
+}
+if (Math.Sqrt(y)==x)
+{
+    Console.WriteLine("{0} является квадратом {1}",y,x);
+}
+}
+else
+{
+    Console.WriteLine("Числа не являются квадратами друг-друга");
+}
+
