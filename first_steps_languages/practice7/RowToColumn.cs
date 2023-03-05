e53c9ef4b8ab5ef01d08d1a6d4ebaf7789d40d73
ins1der23 <m.zadorin@uopt.ru>
2023-03-06 00:09:32 +0500

    FS Lang practice_7 RowsToColumns started, main interface created

---
+++ first_steps_languages/practice7/RowToColumn.cs
@@ -0,0 +1,52 @@
+public class RowToColumn
+{
+    public static int GetInteger(string text)
+    {
+        int num = 0;
+        bool flag = true;
+        do
+        {
+            Console.Write($"{text}: ");
+            flag = int.TryParse(Console.ReadLine(), out num);
+        } while (!flag);
+        return num;
+    }
+    public static int BoundInput(int check, string message, int bound = 0)
+    {
+        bool flag = true;
+        flag = (check > bound);
+        while (!flag)
+        {
+            Console.WriteLine(message);
+            check = GetInteger("Введите еще раз ");
+            flag = (check > bound);
+        }
+        return check;
+    }
+    public static bool EqualCheck(int first, int second)
+    {
+        return first == second;
+    }
+
+    public static (int, int) SquareMatrixSizes(string message)
+    {
+        int rows = 0;
+        int columns = 0;
+        Console.WriteLine(message);
+        do
+        {
+            rows = GetInteger("Введите количество строк");
+            rows = BoundInput(rows, "Количество строк не может быть меньше 1", 0);
+            columns = GetInteger("Введите количество столбцов");
+            columns = BoundInput(columns, "Количество столбцов не может быть меньше 1", 0);
+            if (!EqualCheck(rows, columns)) Console.WriteLine("Для замены количество строк и столбцов должно быть равным");
+        } while (!EqualCheck(rows, columns));
+        return (rows, columns);
+    }
+
+
+
+
+
+}
+
