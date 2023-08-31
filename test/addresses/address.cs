86bcaf4a47841e9293d0c07b097e838df69449a9
ins1der23 <m.zadorin@uopt.ru>
2023-08-31 18:06:06 +0500

    Test QueryMultiple done

--- test/addresses/address.cs
+++ test/addresses/address.cs
@@ -17,7 +17,7 @@ namespace Models
         public virtual Location Location { get; set; }
 
         public int StreetId { get; set; }
-        public virtual Street Street { get; set;}
+        public virtual Street Street { get; set; }
         public string? HouseNum { get; set; }
 
         public Address()
@@ -29,7 +29,7 @@ namespace Models
             Street = new();
         }
 
-        public override string ToString() => $"ID:{Id}, {City.Name}, {District.Name}, {Location.Name}, {Street.Name}, {HouseNum}";
+        public override string ToString() => $"ID:{Id}, {City.Name}, {Location.Name}, {Street.Name}, {HouseNum}";
     }
 
     class Addresses
@@ -44,30 +44,40 @@ namespace Models
             await user.ConnectAsync();
             if (user.IsConnect)
             {
-                var sql = @"select s.id, s.name
-                            from streets as s
-                            where 1 = 1;
-                            select
-                            a.Id, a.streetId, a.houseNum
-                            from addresses as a
-                            where 1 = 1;";
+                string sql = @"select c.id, c.name from cities as c;
+                            select d.id, d.name from districts as d;
+                            select l.id, l.name from locations as l;
+                            select s.id, s.name from streets as s;
+                            select a.Id, a.cityId, a.districtId, a.locationId, a.streetId, a.houseNum
+                            from addresses as a";
 
-
-                using (var temp = user.Connection.QueryMultiple(sql))
+                using (var temp = await user.Connection.QueryMultipleAsync(sql))
                 {
+                    var cities = temp.Read<City>();
+                    var districts = temp.Read<District>();
+                    var locations = temp.Read<Location>();
                     var streets = temp.Read<Street>();
                     var addresses = temp.Read<Address>();
                     addresses = addresses.Select(x => new Address
                     {
                         Id = x.Id,
+                        CityId = x.CityId,
+                        City = cities.Where(c => c.Id == x.CityId).First(),
+                        DistrictId = x.DistrictId,
+                        District = districts.Where(d => d.Id == x.DistrictId).First(),
+                        LocationId = x.LocationId,
+                        Location = locations.Where(l => l.Id == x.LocationId).First(),
                         StreetId = x.StreetId,
-                        HouseNum = x.HouseNum,
-                        Street = streets.Where(y => y.Id == x.StreetId).FirstOrDefault(),
+                        Street = streets.Where(s => s.Id == x.StreetId).First(),
+                        HouseNum = x.HouseNum
                     });
                     AddressList = addresses.ToList();
                 }
 
-               // var temp = await user.Connection.QueryAsync<Address>(selectQuery);
+
+
+
+                // var temp = await user.Connection.QueryAsync<Address>(selectQuery);
 
 
                 // AddressList.ToList();
